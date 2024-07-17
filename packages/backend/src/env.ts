d3d3132a44681abb85577c6e5adc5ab1f770a2c4
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2024-07-17 22:27:22 +0900

    backend: 未使用の環境変数設定を削除

--- packages/backend/src/env.ts
+++ packages/backend/src/env.ts
@@ -6,7 +6,6 @@
 const envOption = {
 	onlyQueue: false,
 	onlyServer: false,
-	noDaemons: false,
 	disableClustering: false,
 	verbose: false,
 	withLogTime: false,
@@ -19,6 +18,5 @@ for (const key of Object.keys(envOption) as (keyof typeof envOption)[]) {
 
 if (process.env.NODE_ENV === 'test') envOption.disableClustering = true;
 if (process.env.NODE_ENV === 'test') envOption.quiet = true;
-if (process.env.NODE_ENV === 'test') envOption.noDaemons = true;
 
 export { envOption };
