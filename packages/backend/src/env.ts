230655b62a992f547cff5076237bcecf481aaf6a
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2024-07-21 13:58:25 +0900

    backend: `envOption`を整理

--- packages/backend/src/env.ts
+++ packages/backend/src/env.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-const envOption = {
+export const envOption = {
 	isTest: process.env['NODE_ENV'] === 'test',
 	isDevelopment: process.env['NODE_ENV'] === 'development',
 	isProduction: process.env['NODE_ENV'] === 'production',
@@ -19,16 +19,11 @@ const envOption = {
 
 	MK_ONLY_QUEUE: process.env['MK_ONLY_QUEUE'] !== undefined,
 	MK_ONLY_SERVER: process.env['MK_ONLY_SERVER'] !== undefined,
-	MK_QUIET: process.env['MK_QUIET'] !== undefined,
+	MK_QUIET:
+		process.env['NODE_ENV'] === 'test' || process.env['MK_QUIET'] !== undefined,
 	MK_VERBOSE: process.env['MK_VERBOSE'] !== undefined,
 	MK_WITH_LOG_TIME: process.env['MK_WITH_LOG_TIME'] !== undefined,
 
 	FORCE_FOLLOW_REMOTE_USER_FOR_TESTING:
 		process.env['FORCE_FOLLOW_REMOTE_USER_FOR_TESTING'] === 'true',
-};
-
-if (envOption.isTest) {
-	envOption.MK_QUIET = true;
-}
-
-export { envOption };
+} as const;
