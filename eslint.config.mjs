c1c6a15c4b152d4e08247f6fed19cff1595cb330
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2024-07-25 20:23:35 +0900

    ESLintの設定を作り直した

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,18 @@
+// @ts-check
+
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+	// ESLint config
 	eslint.configs.recommended,
+
+	// TypeScript support
 	...tseslint.configs.strictTypeChecked,
 	{
 		languageOptions: {
 			parserOptions: {
-				project: true,
+				EXPERIMENTAL_useProjectService: true, // https://typescript-eslint.io/blog/announcing-typescript-eslint-v8-beta#project-service
 				tsconfigRootDir: import.meta.dirname,
 			},
 		},
