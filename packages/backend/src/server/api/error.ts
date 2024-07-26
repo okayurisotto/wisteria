4a48abd91d14a7c8b232ca7ea69bc47748a8fd2f
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2024-07-26 19:32:38 +0900

    backend: `ApiError`を発展

--- packages/backend/src/server/api/error.ts
+++ packages/backend/src/server/api/error.ts
@@ -3,26 +3,35 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-type E = { message: string, code: string, id: string, kind?: 'client' | 'server' | 'permission', httpStatusCode?: number };
+type E = {
+	message: string;
+	code: string;
+	id: string;
+	kind?: 'client' | 'server' | 'permission';
+	httpStatusCode?: number;
+};
+
+const INTERNAL_ERROR = {
+	message: 'Internal error occurred. Please contact us if the error persists.',
+	code: 'INTERNAL_ERROR',
+	id: '5d37dbcb-891e-41ca-a3d6-e690c97775ac',
+	kind: 'server',
+	httpStatusCode: 500,
+} as const satisfies E;
 
 export class ApiError extends Error {
-	public message: string;
-	public code: string;
-	public id: string;
-	public kind: string;
-	public httpStatusCode?: number;
-	public info?: any;
+	public override readonly message: string;
+	public readonly code: string;
+	public readonly id: string;
+	public readonly kind: string;
+	public readonly httpStatusCode: number | undefined;
+	public readonly info: unknown;
 
-	constructor(err?: E | null | undefined, info?: any | null | undefined) {
-		if (err == null) err = {
-			message: 'Internal error occurred. Please contact us if the error persists.',
-			code: 'INTERNAL_ERROR',
-			id: '5d37dbcb-891e-41ca-a3d6-e690c97775ac',
-			kind: 'server',
-			httpStatusCode: 500,
-		};
+	public constructor(err?: E | null | undefined, info?: unknown) {
+		err = INTERNAL_ERROR;
 
 		super(err.message);
+
 		this.message = err.message;
 		this.code = err.code;
 		this.id = err.id;
