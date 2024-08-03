9543dabb70302461093319698c8703a7b6881e64
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2024-08-04 02:35:39 +0900

    backend: `tmp`を更新＆使用箇所をリファクタリング

--- packages/backend/src/misc/create-temp.ts
+++ packages/backend/src/misc/create-temp.ts
@@ -6,25 +6,28 @@
 import { envOption } from '@/env.js';
 import * as tmp from 'tmp';
 
-export function createTemp(): Promise<[string, () => void]> {
-	return new Promise<[string, () => void]>((res, rej) => {
-		tmp.file((e, path, fd, cleanup) => {
-			if (e) return rej(e);
-			res([path, envOption.isProduction ? cleanup : () => {}]);
+export const createTemp = (): Promise<[string, () => void]> => {
+	return new Promise<[string, () => void]>((resolve, reject) => {
+		tmp.file((err, path, _fd, cleanup) => {
+			if (err !== null) {
+				reject(err);
+				return;
+			}
+
+			resolve([path, envOption.isProduction ? cleanup : () => {}]);
 		});
 	});
-}
+};
 
-export function createTempDir(): Promise<[string, () => void]> {
-	return new Promise<[string, () => void]>((res, rej) => {
-		tmp.dir(
-			{
-				unsafeCleanup: true,
-			},
-			(e, path, cleanup) => {
-				if (e) return rej(e);
-				res([path, envOption.isProduction ? cleanup : () => {}]);
-			},
-		);
+export const createTempDir = (): Promise<[string, () => void]> => {
+	return new Promise<[string, () => void]>((resolve, reject) => {
+		tmp.dir({ unsafeCleanup: true }, (err, path, cleanup) => {
+			if (err !== null) {
+				reject(err);
+				return;
+			}
+
+			resolve([path, envOption.isProduction ? cleanup : () => {}]);
+		});
 	});
-}
+};
