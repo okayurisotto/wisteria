80d194f304b86377efac3410e0ecc24c10eda02b
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2024-08-12 10:29:22 +0900

    backend: `omitSearch`がHTTPへリダイレクトしてしまう問題を修正
    8a207a184708186ae14ad519eee9f5ce9bc14c0d で混入。
    
    WisteriaはリバースプロキシによってHTTPSとして公開されるため、Honoの`c.req.url`は`http://`で始まるURLとなる。これにより`http://`で始まるURLにリダイレクトされることになり、結果としてMixed ContentとしてWebブラウザ側でエラーとなってしまう。この修正では、相対URLとしてリダイレクト先を指定することでこの問題を回避した。

--- packages/backend/src/server/omitSearch.ts
+++ packages/backend/src/server/omitSearch.ts
@@ -2,10 +2,9 @@ import type { MiddlewareHandler } from 'hono';
 
 export const omitSearch: MiddlewareHandler = async (c, next) => {
 	const url = new URL(c.req.url);
-	url.search = '';
 
-	if (url.href !== c.req.url) {
-		return c.redirect(url.href, 301);
+	if (url.search !== '') {
+		return c.redirect(url.pathname, 301);
 	}
 
 	await next();
