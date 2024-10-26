f32c0d8d567cbd319fda8cee6d274adb551915a8
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2024-10-26 17:26:24 +0900

    frontend: デッドコードを削除

--- packages/frontend/src/components/MkCwButton.vue
+++ packages/frontend/src/components/MkCwButton.vue
@@ -11,7 +11,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 import { computed } from 'vue';
 import * as Misskey from 'misskey-js';
 import type { PollEditorModelValue } from '@/components/MkPollEditor.vue';
-import { concat } from '@/scripts/array.js';
 import { i18n } from '@/i18n.js';
 import MkButton from '@/components/MkButton.vue';
 
@@ -28,12 +27,12 @@ const emit = defineEmits<{
 }>();
 
 const label = computed(() => {
-	return concat([
+	return [
 		props.text ? [i18n.tsx._cw.chars({ count: props.text.length })] : [],
 		props.renote ? [i18n.ts.quote] : [],
 		props.files && props.files.length !== 0 ? [i18n.tsx._cw.files({ count: props.files.length })] : [],
 		props.poll != null ? [i18n.ts.poll] : [],
-	] as string[][]).join(' / ');
+	].flat().join(' / ');
 });
 
 function toggle() {
