f56e510119152528693b23882f4b70f4d2cbb4f4
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2024-11-09 12:37:40 +0900

    Node.js v22.11.0へ更新

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 # syntax = docker/dockerfile:1.4
 
-ARG IMAGE_TAG=20.15.1-bullseye
+ARG IMAGE_TAG=22.11.0-bullseye
 
 # ----------------------------------------------------------
 # Fetch dependencies
--- scripts/tarball.mjs
+++ scripts/tarball.mjs
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'node:url';
 import glob from 'fast-glob';
 import walk from 'ignore-walk';
 import { Pack } from 'tar';
-import meta from '../package.json' assert { type: 'json' };
+import meta from '../package.json' with { type: 'json' };
 
 const cwd = fileURLToPath(new URL('..', import.meta.url));
 const ignore = [
