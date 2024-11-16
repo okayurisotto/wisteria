b34b53cb5d5e47870f7989a1964b7ebe497b1cb1
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2024-11-17 08:45:28 +0900

    locales: `index.js`を改修

--- packages/locales/index.js
+++ packages/locales/index.js
@@ -3,15 +3,8 @@
  */
 
 import * as fs from 'node:fs';
+import * as path from 'node:path';
 import * as yaml from 'js-yaml';
-
-const merge = (...args) => args.reduce((a, c) => ({
-	...a,
-	...c,
-	...Object.entries(a)
-		.filter(([k]) => c && typeof c[k] === 'object')
-		.reduce((a, [k, v]) => (a[k] = merge(v, c[k]), a), {})
-}), {});
 
 const languages = [
 	'ar-SA',
@@ -48,40 +41,81 @@ const primaries = {
 	'zh': 'CN',
 };
 
+/**
+ * @param  {Record<PropertyKey, unknown>[]} args
+ * @returns {Record<PropertyKey, unknown>}
+ */
+const merge = (...args) => {
+	return args.reduce((prev, current) => ({
+		...prev,
+		...current,
+		...Object.entries(prev)
+			.filter(([k]) => current && typeof current[k] === 'object')
+			.reduce((a, [k, v]) => (a[k] = merge(v, current[k]), a), {})
+	}), {});
+};
+
 // 何故か文字列にバックスペース文字が混入することがあり、YAMLが壊れるので取り除く
-const clean = (text) => text.replace(new RegExp(String.fromCodePoint(0x08), 'g'), '');
+/**
+ * @param {string} text
+ */
+const clean = (text) => {
+	return text.replace(new RegExp(String.fromCodePoint(0x08), 'g'), '');
+};
+
+/**
+ * 空文字列が入ることがあり、フォールバックが動作しなくなるのでプロパティごと消す
+ * @param {Record<string, unknown>} obj
+ */
+const removeEmpty = (obj) => {
+	for (const [k, v] of Object.entries(obj)) {
+		if (v === '') {
+			delete obj[k];
+		} else if (typeof v === 'object') {
+			removeEmpty(v);
+		}
+	}
+	return obj;
+};
 
 export function build() {
-	const locales = languages.reduce((a, c) => (a[c] = yaml.load(clean(fs.readFileSync(new URL(`${c}.yml`, import.meta.url), 'utf-8'))) || {}, a), {});
+	const locales = languages.reduce((prev, current) => {
+		const filename = path.join(import.meta.dirname, `${current}.yml`);
+		const content = fs.readFileSync(filename, 'utf-8');
+		prev[current] = yaml.load(clean(content)) ?? {};
+		return prev;
+	}, {});
 
-	// 空文字列が入ることがあり、フォールバックが動作しなくなるのでプロパティごと消す
-	const removeEmpty = (obj) => {
-		for (const [k, v] of Object.entries(obj)) {
-			if (v === '') {
-				delete obj[k];
-			} else if (typeof v === 'object') {
-				removeEmpty(v);
-			}
-		}
-		return obj;
-	};
 	removeEmpty(locales);
 
 	return Object.entries(locales)
-		.reduce((a, [k, v]) => (a[k] = (() => {
-			const [lang] = k.split('-');
-			switch (k) {
-				case 'ja-JP': return v;
-				case 'ja-KS':
-				case 'en-US': return merge(locales['ja-JP'], v);
-				default: return merge(
-					locales['ja-JP'],
-					locales['en-US'],
-					locales[`${lang}-${primaries[lang]}`] ?? {},
-					v
-				);
-			}
-		})(), a), {});
+		.reduce((prev, [key, value]) => {
+			prev[key] = (() => {
+				const [lang] = key.split('-');
+
+				switch (key) {
+					case 'ja-JP': {
+						return value;
+					}
+					case 'ja-KS': {
+						return merge(locales['ja-JP'], value);
+					}
+					case 'en-US': {
+						return merge(locales['ja-JP'], value);
+					}
+					default: {
+						return merge(
+							locales['ja-JP'],
+							locales['en-US'],
+							locales[`${lang}-${primaries[lang]}`] ?? {},
+							value,
+						);
+					}
+				}
+			})();
+
+			return prev;
+		}, {});
 }
 
 export default build();
