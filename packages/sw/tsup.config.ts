7a6980123f05490ac1496440dc51e23d4325e1e9
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2025-01-11 16:22:16 +0900

    sw: ビルドできていなかった問題を修正

--- packages/sw/tsup.config.ts
+++ packages/sw/tsup.config.ts
@@ -4,9 +4,10 @@ import meta from '../../package.json' assert { type: 'json' };
 export default defineConfig({
 	bundle: true,
 	entry: ['./src/sw.ts'],
-	format: 'esm',
+	format: 'iife',
 	minify: true,
 	outDir: 'built',
+	outExtension: () => ({ js: '.js' }),
 	define: {
 		_DEV_: JSON.stringify(process.env.NODE_ENV !== 'production'),
 		_VERSION_: JSON.stringify(meta.version),
