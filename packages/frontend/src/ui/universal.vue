fe9e4af6bce227f3e4572e75aff3ee3005fe33c6
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2025-05-23 19:45:27 +0900

    frontend: universal UIのナビゲーションフッターの各ボタンをrounded-fullに戻した

--- packages/frontend/src/ui/universal.vue
+++ packages/frontend/src/ui/universal.vue
@@ -446,10 +446,10 @@ $widgets-hide-threshold: 1090px;
 .navButton {
 	align-items: center;
 	background: var(--panel);
-	border-radius: var(--rounded);
+	border-radius: var(--rounded-full);
 	color: var(--fg);
 	display: flex;
-	flex: 1 0 50px;
+	flex: 0 0 4rem;
 	height: 4rem;
 	justify-content: center;
 	position: relative;
