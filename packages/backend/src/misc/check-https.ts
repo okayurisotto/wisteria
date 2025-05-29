98939e4f716629f278cfe348782fa33aa2248191
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2025-05-29 23:43:30 +0900

    backend: refactor `checkHttps()`

--- packages/backend/src/misc/check-https.ts
+++ packages/backend/src/misc/check-https.ts
@@ -5,7 +5,22 @@
 
 import { envOption } from '@/env.js';
 
-export function checkHttps(url: string): boolean {
-	return url.startsWith('https://') ||
-		(url.startsWith('http://') && !envOption.isProduction);
-}
+/**
+ * URLのプロトコルがHTTPSであるかどうかを確認します。
+ * 開発環境でのみHTTPも許容し、HTTPSであるものとして`true`を返します。
+ */
+export const checkHttps = (urlText: string): boolean => {
+	try {
+		const url = new URL(urlText);
+
+		if (url.protocol === 'https:') {
+			return true;
+		} else if (url.protocol === 'http:' && !envOption.isProduction) {
+			return true;
+		} else {
+			return false;
+		}
+	} catch {
+		return false;
+	}
+};
