fda76097087bca687066ebde405338573365fb2e
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2025-05-30 14:04:06 +0900

    frontend: ナビゲーションバーから`about`を削除

--- packages/frontend/src/navbar.ts
+++ packages/frontend/src/navbar.ts
@@ -7,7 +7,7 @@ import { computed, reactive } from 'vue';
 import { clearCache } from './scripts/clear-cache.js';
 import { $i } from '@/account.js';
 import { miLocalStorage } from '@/local-storage.js';
-import { openInstanceMenu, openToolsMenu } from '@/ui/_common_/common.js';
+import { openToolsMenu } from '@/ui/_common_/common.js';
 import { lookup } from '@/scripts/lookup.js';
 import * as os from '@/os.js';
 import { i18n } from '@/i18n.js';
@@ -120,13 +120,6 @@ export const navbarItemDef = reactive({
 					unisonReload();
 				},
 			}], ev.currentTarget ?? ev.target);
-		},
-	},
-	about: {
-		title: i18n.ts.about,
-		icon: 'ti ti-info-circle',
-		action: (ev) => {
-			openInstanceMenu(ev);
 		},
 	},
 	tools: {
