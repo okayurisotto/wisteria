f24b4e392ab1550c2c2d53cc69380b297c056fe6
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2025-06-12 16:36:19 +0900

    frontend: `MkUrlPreview`の改修
    - YouTubeやTwitterの埋め込みへの対応を削除
    - CSSの調整

--- packages/frontend/src/components/MkUrlPreview.vue
+++ packages/frontend/src/components/MkUrlPreview.vue
@@ -4,95 +4,58 @@ SPDX-License-Identifier: AGPL-3.0-only
 -->
 
 <template>
-<template v-if="player.url && playerEnabled">
-	<div
-		:class="$style.player"
-		:style="player.width ? `padding: ${(player.height || 0) / player.width * 100}% 0 0` : `padding: ${(player.height || 0)}px 0 0`"
-	>
-		<iframe
-			v-if="player.url.startsWith('http://') || player.url.startsWith('https://')"
-			sandbox="allow-popups allow-scripts allow-storage-access-by-user-activation allow-same-origin"
-			scrolling="no"
-			:allow="player.allow == null ? 'autoplay;encrypted-media;fullscreen' : player.allow.filter(x => ['autoplay', 'clipboard-write', 'fullscreen', 'encrypted-media', 'picture-in-picture', 'web-share'].includes(x)).join(';')"
-			:class="$style.playerIframe"
-			:src="player.url + (player.url.match(/\?/) ? '&autoplay=1&auto_play=1' : '?autoplay=1&auto_play=1')"
-			:style="{ border: 0 }"
-		></iframe>
-		<span v-else>invalid url</span>
-	</div>
-	<div :class="$style.action">
-		<MkButton :small="true" inline @click="playerEnabled = false">
-			<i class="ti ti-x"></i> {{ i18n.ts.disablePlayer }}
-		</MkButton>
-	</div>
-</template>
-<template v-else-if="tweetId && tweetExpanded">
-	<div ref="twitter">
-		<iframe
-			ref="tweet"
-			allow="fullscreen;web-share"
-			sandbox="allow-popups allow-popups-to-escape-sandbox allow-scripts allow-same-origin"
-			scrolling="no"
-			:style="{ position: 'relative', width: '100%', height: `${tweetHeight}px`, border: 0 }"
-			:src="`https://platform.twitter.com/embed/index.html?embedId=${embedId}&amp;hideCard=false&amp;hideThread=false&amp;lang=en&amp;theme=${defaultStore.state.darkMode ? 'dark' : 'light'}&amp;id=${tweetId}`"
-		></iframe>
-	</div>
-	<div :class="$style.action">
-		<MkButton :small="true" inline @click="tweetExpanded = false">
-			<i class="ti ti-x"></i> {{ i18n.ts.close }}
-		</MkButton>
-	</div>
-</template>
-<div v-else>
-	<component :is="self ? 'MkA' : 'a'" :class="[$style.link, { [$style.compact]: compact }]" :[attr]="self ? url.substring(local.length) : url" rel="nofollow noopener" :target="target" :title="url">
-		<article :class="$style.body">
-			<header :class="$style.header">
-				<h1 v-if="unknownUrl" :class="$style.title">{{ url }}</h1>
-				<h1 v-else-if="fetching" :class="$style.title"><MkEllipsis/></h1>
-				<h1 v-else :class="$style.title" :title="title ?? undefined">{{ title }}</h1>
-			</header>
-			<p v-if="unknownUrl" :class="$style.text">{{ i18n.ts.failedToPreviewUrl }}</p>
-			<p v-else-if="fetching" :class="$style.text"><MkEllipsis/></p>
-			<p v-else-if="description" :class="$style.text" :title="description">{{ description.length > 85 ? description.slice(0, 85) + '…' : description }}</p>
-			<footer :class="$style.footer">
-				<img v-if="icon" :class="$style.siteIcon" :src="icon"/>
-				<p v-if="unknownUrl" :class="$style.siteName">{{ requestUrl.host }}</p>
-				<p v-else-if="fetching" :class="$style.siteName"><MkEllipsis/></p>
-				<p v-else :class="$style.siteName" :title="sitename ?? requestUrl.host">{{ sitename ?? requestUrl.host }}</p>
-			</footer>
-		</article>
-		<img
-			v-if="thumbnail && !sensitive && !defaultStore.state.dataSaver.urlPreview"
-			:class="$style.thumbnail"
-			:src="thumbnail"
-		/>
-	</component>
-	<template v-if="showActions">
-		<div v-if="tweetId" :class="$style.action">
-			<MkButton :small="true" inline @click="tweetExpanded = true">
-				<i class="ti ti-brand-x"></i> {{ i18n.ts.expandTweet }}
-			</MkButton>
-		</div>
-		<div v-if="!playerEnabled && player.url" :class="$style.action">
-			<MkButton :small="true" inline @click="playerEnabled = true">
-				<i class="ti ti-player-play"></i> {{ i18n.ts.enablePlayer }}
-			</MkButton>
-			<MkButton v-if="!isMobile" :small="true" inline @click="openPlayer()">
-				<i class="ti ti-picture-in-picture"></i> {{ i18n.ts.openInWindow }}
-			</MkButton>
-		</div>
-	</template>
-</div>
+  <component
+    :is="self ? 'MkA' : 'a'"
+    :class="$style.root"
+    :[attr]="self ? url.substring(local.length) : url"
+    rel="nofollow noopener"
+    :target="target"
+  >
+    <article :class="$style.body">
+      <header>
+        <h1 v-if="fetching" :class="$style.title">
+          <MkEllipsis />
+        </h1>
+        <h1 v-else-if="unknownUrl || title === null" :class="$style.title">
+          {{ url }}
+        </h1>
+        <h1 v-else :class="$style.title" :title="title">
+          {{ title }}
+        </h1>
+      </header>
+      <div>
+        <p v-if="unknownUrl" :class="$style.text">
+          {{ i18n.ts.failedToPreviewUrl }}
+        </p>
+        <p v-else-if="fetching" :class="$style.text">
+          <MkEllipsis />
+        </p>
+        <p v-else-if="description" :class="$style.text" :title="description">
+          {{ description }}
+        </p>
+      </div>
+      <footer :class="$style.footer">
+        <img v-if="icon" :class="$style.siteIcon" :src="icon">
+        <p v-if="unknownUrl" :class="$style.siteName">
+          {{ requestUrl.host }}
+        </p>
+        <p v-else-if="fetching" :class="$style.siteName">
+          <MkEllipsis />
+        </p>
+        <p v-else :class="$style.siteName" :title="sitename ?? requestUrl.host">
+          {{ sitename ?? requestUrl.host }}
+        </p>
+      </footer>
+    </article>
+    <img v-if="thumbnail && !sensitive && !defaultStore.state.dataSaver['urlPreview']" :class="$style.thumbnail" :src="thumbnail">
+  </component>
 </template>
 
 <script lang="ts" setup>
-import { defineAsyncComponent, onDeactivated, onUnmounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import type { summaly } from '@misskey-dev/summaly';
 import { url as local } from '@/config.js';
 import { i18n } from '@/i18n.js';
-import * as os from '@/os.js';
-import { deviceKind } from '@/scripts/device-kind.js';
-import MkButton from '@/components/MkButton.vue';
 import { versatileLang } from '@/scripts/intl-const.js';
 import { defaultStore } from '@/store.js';
 
@@ -109,12 +72,15 @@ const props = withDefaults(defineProps<{
 	showActions: true,
 });
 
-const MOBILE_THRESHOLD = 500;
-const isMobile = ref(deviceKind === 'smartphone' || window.innerWidth <= MOBILE_THRESHOLD);
-
-const self = props.url.startsWith(local);
-const attr = self ? 'to' : 'href';
-const target = self ? null : '_blank';
+const requestUrl = computed(() => {
+	const url = new URL(props.url);
+	if (!['http:', 'https:'].includes(url.protocol)) throw new Error('invalid url');
+	url.hash = '';
+	return url;
+});
+const self = computed(() => props.url.startsWith(local));
+const attr = computed(() => self.value ? 'to' : 'href');
+const target = computed(() => self.value ? null : '_blank');
 const fetching = ref(true);
 const title = ref<string | null>(null);
 const description = ref<string | null>(null);
@@ -122,171 +88,76 @@ const thumbnail = ref<string | null>(null);
 const icon = ref<string | null>(null);
 const sitename = ref<string | null>(null);
 const sensitive = ref<boolean>(false);
-const player = ref({
-	url: null,
-	width: null,
-	height: null,
-} as SummalyResult['player']);
-const playerEnabled = ref(false);
-const tweetId = ref<string | null>(null);
-const tweetExpanded = ref(props.detail);
-const embedId = `embed${Math.random().toString().replace(/\D/, '')}`;
-const tweetHeight = ref(150);
 const unknownUrl = ref(false);
 
-onDeactivated(() => {
-	playerEnabled.value = false;
-});
+onMounted(async () => {
+	const url = new URL('/url', location.origin);
+	url.searchParams.set('url', requestUrl.value.href);
+	url.searchParams.set('lang', versatileLang);
 
-const requestUrl = new URL(props.url);
-if (!['http:', 'https:'].includes(requestUrl.protocol)) throw new Error('invalid url');
-
-if (requestUrl.hostname === 'twitter.com' || requestUrl.hostname === 'mobile.twitter.com' || requestUrl.hostname === 'x.com' || requestUrl.hostname === 'mobile.x.com') {
-	const m = requestUrl.pathname.match(/^\/.+\/status(?:es)?\/(\d+)/);
-	if (m) tweetId.value = m[1];
-}
-
-if (requestUrl.hostname === 'music.youtube.com' && requestUrl.pathname.match('^/(?:watch|channel)')) {
-	requestUrl.hostname = 'www.youtube.com';
-}
-
-requestUrl.hash = '';
-
-window.fetch(`/url?url=${encodeURIComponent(requestUrl.href)}&lang=${versatileLang}`)
-	.then(res => {
-		if (!res.ok) {
-			fetching.value = false;
-			unknownUrl.value = true;
-			return;
-		}
-
-		return res.json();
-	})
-	.then((info: SummalyResult) => {
-		if (info.url == null) {
-			fetching.value = false;
-			unknownUrl.value = true;
-			return;
-		}
-
+	const res = await fetch(url);
+	if (!res.ok) {
 		fetching.value = false;
-		unknownUrl.value = false;
+		unknownUrl.value = true;
+		return;
+	}
 
-		title.value = info.title;
-		description.value = info.description;
-		thumbnail.value = info.thumbnail;
-		icon.value = info.icon;
-		sitename.value = info.sitename;
-		player.value = info.player;
-		sensitive.value = info.sensitive ?? false;
-	});
+	const info: SummalyResult = await res.json();
 
-function adjustTweetHeight(message: any) {
-	if (message.origin !== 'https://platform.twitter.com') return;
-	const embed = message.data?.['twttr.embed'];
-	if (embed?.method !== 'twttr.private.resize') return;
-	if (embed?.id !== embedId) return;
-	const height = embed?.params[0]?.height;
-	if (height) tweetHeight.value = height;
-}
+	fetching.value = false;
+	unknownUrl.value = false;
 
-const openPlayer = (): void => {
-	os.popup(defineAsyncComponent(() => import('@/components/MkYouTubePlayer.vue')), {
-		url: requestUrl.href,
-	});
-};
-
-(window as any).addEventListener('message', adjustTweetHeight);
-
-onUnmounted(() => {
-	(window as any).removeEventListener('message', adjustTweetHeight);
+	title.value = info.title;
+	description.value = info.description;
+	thumbnail.value = info.thumbnail;
+	icon.value = info.icon;
+	sitename.value = info.sitename;
+	sensitive.value = info.sensitive ?? false;
 });
 </script>
 
 <style lang="scss" module>
-.player {
-	position: relative;
-	width: 100%;
-}
+.root {
+	contain: strict;
+	container-type: inline-size;
 
-.disablePlayer {
-	position: absolute;
-	top: -1.5em;
-	right: 0;
-	font-size: 1em;
-	width: 1.5em;
-	height: 1.5em;
-	padding: 0;
-	margin: 0;
-	color: var(--fg);
-	background: rgba(128, 128, 128, 0.2);
-	opacity: 0.7;
-
-	&:hover {
-		opacity: 0.9;
-	}
-}
-
-.playerIframe {
-	height: 100%;
-	left: 0;
-	position: absolute;
-	top: 0;
-	width: 100%;
-}
-
-.link {
-	font-size: 14px;
-	box-shadow: 0 0 0 1px var(--divider);
-	border-radius: var(--rounded);
 	align-items: center;
+	border-radius: var(--rounded);
+	border: 1px solid var(--divider);
 	box-sizing: border-box;
 	display: flex;
 	height: 100px;
+	transition: border-color .1s;
 
 	&:hover {
+		border-color: var(--accent);
 		text-decoration: none;
-		border-color: rgba(0, 0, 0, 0.2);
-
-		> .body > .header > .title {
-			text-decoration: underline;
-		}
 	}
-
-	&.compact {
-		> .body {
-			> .header .title, .text, .footer {
-				overflow: hidden;
-				white-space: nowrap;
-				text-overflow: ellipsis;
-			}
-		}
-	}
-}
-
-.thumbnail {
-	height: 100%;
-	border-radius: 0 var(--rounded) var(--rounded) 0;
-	max-width: 191px; // 1.91 : 1
-	object-fit: cover;
 }
 
 .body {
 	display: flex;
 	flex-direction: column;
-	flex-grow: 1;
-	gap: 8px;
+	flex: 1;
 	overflow-x: hidden;
-	padding-inline: 16px;
+	padding: var(--marginHalf);
+	row-gap: 8px;
+}
+
+.title,
+.text,
+.siteName {
+	margin: 0;
+	overflow-x: hidden;
+	text-overflow: ellipsis;
+	text-wrap: nowrap;
 }
 
 .title {
-	margin: 0;
 	font-size: 1em;
 }
 
 .text {
-	margin: 0;
 	font-size: 0.8em;
 }
 
@@ -297,47 +168,20 @@ onUnmounted(() => {
 
 .siteIcon {
 	height: 16px;
-	aspect-ratio: 1 / 1;
+	width: 16px;
 }
 
 .siteName {
-	margin: 0;
-	color: var(--urlPreviewInfo);
 	font-size: 0.8em;
 }
 
-.action {
-	display: flex;
-	gap: 6px;
-	flex-wrap: wrap;
-	margin-top: 6px;
-}
+.thumbnail {
+	height: 100px;
+	max-width: min(191px, 30%);
+	object-fit: cover;
 
-@container (max-width: 360px) {
-	.link {
-		font-size: 10px;
-		height: 75px;
-
-		&.compact {
-			> .body {
-				padding: 4px;
-				gap: 2px;
-			}
-		}
-	}
-
-	.thumbnail {
-		min-width: 75px;
-	}
-
-	.body {
-		padding: 8px;
-		gap: 4px;
-	}
-
-	.siteIcon {
-		width: 12px;
-		height: 12px;
+	@container (width < 350px) {
+		display: none;
 	}
 }
 </style>
