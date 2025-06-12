d7e1549c0582922bb324d91bf9c036dcfc67202f
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2025-06-12 17:32:15 +0900

    frontend: refactor `getNoteClipMenu`
    クリップが1つもないときのメニューの項目を整理

--- packages/frontend/src/scripts/get-note-menu.ts
+++ packages/frontend/src/scripts/get-note-menu.ts
@@ -35,61 +35,67 @@ export async function getNoteClipMenu(props: {
 	const appearNote = isRenote ? props.note.renote as Misskey.entities.Note : props.note;
 
 	const clips = await clipsCache.fetch();
-	const menu: MenuItem[] = [...clips.map(clip => ({
-		text: clip.name,
-		action: () => {
-			os.promiseDialog(
-				misskeyApi('clips/add-note', { clipId: clip.id, noteId: appearNote.id }),
-				null,
-				async (err) => {
-					if (err.id === '734806c4-542c-463a-9311-15c512803965') {
-						const confirm = await os.confirm({
-							type: 'warning',
-							text: i18n.tsx.confirmToUnclipAlreadyClippedNote({ name: clip.name }),
-						});
-						if (!confirm.canceled) {
-							os.apiWithDialog('clips/remove-note', { clipId: clip.id, noteId: appearNote.id });
-							if (props.currentClip?.id === clip.id) props.isDeleted.value = true;
-						}
-					} else {
-						os.alert({
-							type: 'error',
-							text: err.message + '\n' + err.id,
-						});
-					}
+	const menu: MenuItem[] = [
+		...(clips.length > 0 ? [
+			...clips.map(clip => ({
+				text: clip.name,
+				action: () => {
+					os.promiseDialog(
+						misskeyApi('clips/add-note', { clipId: clip.id, noteId: appearNote.id }),
+						null,
+						async (err) => {
+							if (err.id === '734806c4-542c-463a-9311-15c512803965') {
+								const confirm = await os.confirm({
+									type: 'warning',
+									text: i18n.tsx.confirmToUnclipAlreadyClippedNote({ name: clip.name }),
+								});
+								if (!confirm.canceled) {
+									os.apiWithDialog('clips/remove-note', { clipId: clip.id, noteId: appearNote.id });
+									if (props.currentClip?.id === clip.id) props.isDeleted.value = true;
+								}
+							} else {
+								os.alert({
+									type: 'error',
+									text: err.message + '\n' + err.id,
+								});
+							}
+						},
+					);
 				},
-			);
+			})),
+			{ type: 'divider' },
+		] as const : []),
+		{
+			icon: 'ti ti-plus',
+			text: i18n.ts.createNew,
+			action: async () => {
+				const { canceled, result } = await os.form(i18n.ts.createNewClip, {
+					name: {
+						type: 'string',
+						label: i18n.ts.name,
+					},
+					description: {
+						type: 'string',
+						required: false,
+						multiline: true,
+						label: i18n.ts.description,
+					},
+					isPublic: {
+						type: 'boolean',
+						label: i18n.ts.public,
+						default: false,
+					},
+				});
+				if (canceled) return;
+
+				const clip = await os.apiWithDialog('clips/create', result);
+
+				clipsCache.delete();
+
+				os.apiWithDialog('clips/add-note', { clipId: clip.id, noteId: appearNote.id });
+			},
 		},
-	})), { type: 'divider' }, {
-		icon: 'ti ti-plus',
-		text: i18n.ts.createNew,
-		action: async () => {
-			const { canceled, result } = await os.form(i18n.ts.createNewClip, {
-				name: {
-					type: 'string',
-					label: i18n.ts.name,
-				},
-				description: {
-					type: 'string',
-					required: false,
-					multiline: true,
-					label: i18n.ts.description,
-				},
-				isPublic: {
-					type: 'boolean',
-					label: i18n.ts.public,
-					default: false,
-				},
-			});
-			if (canceled) return;
-
-			const clip = await os.apiWithDialog('clips/create', result);
-
-			clipsCache.delete();
-
-			os.apiWithDialog('clips/add-note', { clipId: clip.id, noteId: appearNote.id });
-		},
-	}];
+	];
 
 	return menu;
 }
