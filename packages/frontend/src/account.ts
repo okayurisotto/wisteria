47f31bdf57f67c8205b9caed17d398ef6f1daa19
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2025-08-22 16:02:59 +0900

    frontend: `document.cookie`を操作するコードを削除

--- packages/frontend/src/account.ts
+++ packages/frontend/src/account.ts
@@ -208,7 +208,6 @@ export async function login(token: Account['token'], redirect?: string) {
 			throw reason;
 		});
 	miLocalStorage.setItem('account', JSON.stringify(me));
-	document.cookie = `token=${token}; path=/; max-age=31536000`; // bull dashboardの認証とかで使う
 	await addAccount(me.id, token);
 
 	if (redirect) {
