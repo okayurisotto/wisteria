8d3b77beb01a580d962e212102ec1586054d4ec7
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2025-10-14 12:38:57 +0900

    frontend: drop blurhash

--- packages/frontend/src/components/MkImgWithBlurhash.vue
+++ packages/frontend/src/components/MkImgWithBlurhash.vue
@@ -4,50 +4,18 @@ SPDX-License-Identifier: AGPL-3.0-only
 -->
 
 <template>
-<div ref="root" :class="[$style.root, { [$style.cover]: cover }]" :title="title ?? ''">
-	<img v-show="hide" key="blurimg" ref="blurimg" :class="$style.img" :width="imgWidth" :height="imgHeight" :title="title ?? undefined"/>
-	<img v-show="!hide" key="img" ref="img" :height="imgHeight" :width="imgWidth" :class="$style.img" :src="src ?? undefined" :title="title ?? undefined" :alt="alt ?? undefined" loading="eager" decoding="async"/>
-</div>
+	<div ref="root" :class="[$style.root, { [$style.cover]: cover }]" :title="title ?? ''">
+		<span v-show="props.forceBlurhash" :class="$style.avgColor"
+			:style="{ background: avgColor, width: imgWidth + 'px', height: imgHeight + 'px' }"></span>
+		<img v-show="!props.forceBlurhash" :class="$style.img" :height="imgHeight" :width="imgWidth" :src="src ?? undefined"
+			:alt="alt ?? undefined" loading="eager" decoding="async" />
+	</div>
 </template>
 
-<script lang="ts">
-import DrawBlurhash from '@/workers/draw-blurhash?worker';
-import TestWebGL2 from '@/workers/test-webgl2?worker';
-import { WorkerMultiDispatch } from '@/scripts/worker-multi-dispatch.js';
-import { extractAvgColorFromBlurhash } from '@/scripts/extract-avg-color-from-blurhash.js';
-
-const canvasPromise = new Promise<WorkerMultiDispatch | OffscreenCanvas | HTMLCanvasElement>(resolve => {
-	// テスト環境で Web Worker インスタンスは作成できない
-	if (import.meta.env.MODE === 'test') {
-		const canvas = document.createElement('canvas');
-		canvas.width = 64;
-		canvas.height = 64;
-		resolve(canvas);
-		return;
-	}
-	const testWorker = new TestWebGL2();
-	testWorker.addEventListener('message', event => {
-		if (event.data.result) {
-			const workers = new WorkerMultiDispatch(
-				() => new DrawBlurhash(),
-				Math.min(navigator.hardwareConcurrency - 1, 4),
-			);
-			resolve(workers);
-			if (_DEV_) console.log('WebGL2 in worker is supported!');
-		} else {
-			resolve(new OffscreenCanvas(64, 64));
-			if (_DEV_) console.log('WebGL2 in worker is not supported...');
-		}
-		testWorker.terminate();
-	});
-});
-</script>
-
 <script lang="ts" setup>
-import { computed, nextTick, onMounted, onUnmounted, shallowRef, watch, ref } from 'vue';
-import { v4 as uuid } from 'uuid';
-import { render } from 'buraha';
-import { defaultStore } from '@/store.js';
+import { computed, useTemplateRef } from 'vue';
+import { extractAvgColorFromBlurhash } from '@/scripts/extract-avg-color-from-blurhash.js';
+import { useElementSize } from '@vueuse/core';
 
 const props = withDefaults(defineProps<{
 	src?: string | null;
@@ -58,7 +26,6 @@ const props = withDefaults(defineProps<{
 	width?: number;
 	cover?: boolean;
 	forceBlurhash?: boolean;
-	onlyAvgColor?: boolean; // 軽量化のためにBlurhashを使わずに平均色だけを描画
 }>(), {
 	src: null,
 	alt: '',
@@ -67,142 +34,23 @@ const props = withDefaults(defineProps<{
 	width: 64,
 	cover: true,
 	forceBlurhash: false,
-	onlyAvgColor: false,
 });
 
-const viewId = uuid();
-const blurimg = shallowRef<HTMLImageElement>();
-const root = shallowRef<HTMLDivElement>();
-const img = shallowRef<HTMLImageElement>();
-const loaded = ref(false);
-const canvasWidth = ref(64);
-const canvasHeight = ref(64);
-const imgWidth = ref(props.width);
-const imgHeight = ref(props.height);
-const blobTmp = ref<Blob>();
-const blobUrl = ref<string>();
-const hide = computed(() => !loaded.value || props.forceBlurhash);
+const root = useTemplateRef('root');
+const rootSize = useElementSize(root);
 
-function waitForDecode() {
-	if (props.src != null && props.src !== '') {
-		nextTick()
-			.then(() => img.value?.decode())
-			.then(() => {
-				loaded.value = true;
-			}, error => {
-				console.log('Error occurred during decoding image', img.value, error);
-			});
-	} else {
-		loaded.value = false;
-	}
-}
+const imgWidth = computed(() => {
+	return rootSize.width.value ?? 300
+});
 
-watch([() => props.width, () => props.height, root], () => {
+const imgHeight = computed(() => {
 	const ratio = props.width / props.height;
-	if (ratio > 1) {
-		canvasWidth.value = Math.round(64 * ratio);
-		canvasHeight.value = 64;
-	} else {
-		canvasWidth.value = 64;
-		canvasHeight.value = Math.round(64 / ratio);
-	}
-
-	const clientWidth = root.value?.clientWidth ?? 300;
-	imgWidth.value = clientWidth;
-	imgHeight.value = Math.round(clientWidth / ratio);
-}, {
-	immediate: true,
+	return Math.round(imgWidth.value / ratio);
 });
 
-async function drawImage(blob: Blob): Promise<void> {
-	// blurimgがない（mountedされていない）場合はblobTmpに保存しておく
-	if (blurimg.value === undefined) {
-		blobTmp.value = blob;
-		return;
-	}
-
-	// blurimgがあれば描画する
-	const url = URL.createObjectURL(blob);
-	blurimg.value.src = url;
-
-	if (blobUrl.value !== undefined) {
-		URL.revokeObjectURL(blobUrl.value);
-	}
-	blobUrl.value = url;
-}
-
-function drawAvg() {
-	if (blurimg.value === undefined || props.hash == null) return;
-
-	const avgColor = extractAvgColorFromBlurhash(props.hash) ?? '#888';
-	blurimg.value.style.backgroundColor = avgColor;
-}
-
-async function draw() {
+const avgColor = computed(() => {
 	if (props.hash == null) return;
-
-	drawAvg();
-
-	if (props.onlyAvgColor) return;
-
-	const work = await canvasPromise;
-	if (work instanceof WorkerMultiDispatch) {
-		work.postMessage(
-			{
-				id: viewId,
-				hash: props.hash,
-			},
-			undefined,
-		);
-	} else {
-		try {
-			render(props.hash, work);
-			drawImage(await work.convertToBlob());
-		} catch (error) {
-			console.error('Error occurred during drawing blurhash', error);
-		}
-	}
-}
-
-function workerOnMessage(event: MessageEvent) {
-	if (event.data.id !== viewId) return;
-	drawImage(event.data.blob as Blob);
-}
-
-canvasPromise.then(work => {
-	if (work instanceof WorkerMultiDispatch) {
-		work.addListener(workerOnMessage);
-	}
-
-	draw();
-});
-
-watch(() => props.src, () => {
-	waitForDecode();
-});
-
-watch(() => props.hash, () => {
-	draw();
-});
-
-onMounted(() => {
-	// drawImageがmountedより先に呼ばれている場合はここで描画する
-	if (blobTmp.value) {
-		drawImage(blobTmp.value);
-	}
-	waitForDecode();
-});
-
-onUnmounted(() => {
-	if (blobUrl.value) {
-		URL.revokeObjectURL(blobUrl.value)
-	}
-
-	canvasPromise.then(work => {
-		if (work instanceof WorkerMultiDispatch) {
-			work.removeListener(workerOnMessage);
-		}
-	});
+	return extractAvgColorFromBlurhash(props.hash) ?? '#888';
 });
 </script>
 
@@ -213,7 +61,7 @@ onUnmounted(() => {
 	height: 100%;
 
 	&.cover {
-		> .img {
+		>.img {
 			object-fit: cover;
 		}
 	}
@@ -224,5 +72,9 @@ onUnmounted(() => {
 	width: 100%;
 	height: 100%;
 	object-fit: contain;
+}
+
+.avgColor {
+	display: block;
 }
 </style>
