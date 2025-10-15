a1060c98b9923e3e2c0b70d96f38ac3f068e35f9
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2025-10-15 11:31:16 +0900

    frontend: プロフィールページへ遷移するリンクをグリッドナビゲーションから削除

--- packages/frontend/src/navbar.ts
+++ packages/frontend/src/navbar.ts
@@ -101,12 +101,6 @@ export const navbarItemDef = reactive({
 			location.reload();
 		},
 	},
-	profile: {
-		title: i18n.ts.profile,
-		icon: 'ti ti-user',
-		show: computed(() => $i != null),
-		to: `/@${$i?.username}`,
-	},
 	cacheClear: {
 		title: i18n.ts.clearCache,
 		icon: 'ti ti-trash',
