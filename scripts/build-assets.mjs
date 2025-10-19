92ae9f14e4ff6cc0f4c2096483822b37235e84f6
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2025-10-19 13:21:18 +0900

    `build-assets.mjs`で相対パスを使わないように

--- scripts/build-assets.mjs
+++ scripts/build-assets.mjs
@@ -12,41 +12,63 @@ import { loadConfig } from '../packages/backend/built/config.js';
 import { languages } from '../packages/locales/built/index.js';
 import { build as buildTarball } from './tarball.mjs';
 
+const root = path.join(import.meta.dirname, '..');
+
 async function copyBackendViews() {
-	await fs.cp('./packages/backend/src/server/web/views', './packages/backend/built/server/web/views', { recursive: true });
+	await fs.cp(
+		path.join(root, './packages/backend/src/server/web/views'),
+		path.join(root, './packages/backend/built/server/web/views'),
+		{ recursive: true },
+	);
 }
 
 async function copyBackendAssets() {
-	await fs.cp('./packages/backend/src/server/assets', './packages/backend/built/server/assets', { recursive: true });
+	await fs.cp(
+		path.join(root, './packages/backend/src/server/assets'),
+		path.join(root, './packages/backend/built/server/assets'),
+		{ recursive: true },
+	);
 }
 
 async function buildBackendScript() {
-	await fs.mkdir('./packages/backend/built/server/web', { recursive: true });
+	await fs.mkdir(
+		path.join(root, './packages/backend/built/server/web'),
+		{ recursive: true },
+	);
 
 	for (const file of [
-		'./packages/backend/src/server/web/boot.js',
-		'./packages/backend/src/server/web/bios.js',
-		'./packages/backend/src/server/web/cli.js',
+		path.join(root, './packages/backend/src/server/web/boot.js'),
+		path.join(root, './packages/backend/src/server/web/bios.js'),
+		path.join(root, './packages/backend/src/server/web/cli.js'),
 	]) {
 		let source = await fs.readFile(file, { encoding: 'utf-8' });
 		source = source.replaceAll('LANGS', JSON.stringify(Object.keys(languages)));
 		const { code } = await terser.minify(source, { toplevel: true });
-		await fs.writeFile(`./packages/backend/built/server/web/${path.basename(file)}`, code);
+		await fs.writeFile(
+			path.join(root, './packages/backend/built/server/web/', path.basename(file)),
+			code,
+		);
 	}
 }
 
 async function buildBackendStyle() {
-	await fs.mkdir('./packages/backend/built/server/web', { recursive: true });
+	await fs.mkdir(
+		path.join(root, './packages/backend/built/server/web'),
+		{ recursive: true },
+	);
 
 	for (const file of [
-		'./packages/backend/src/server/web/style.css',
-		'./packages/backend/src/server/web/bios.css',
-		'./packages/backend/src/server/web/cli.css',
-		'./packages/backend/src/server/web/error.css',
+		path.join(root, './packages/backend/src/server/web/style.css'),
+		path.join(root, './packages/backend/src/server/web/bios.css'),
+		path.join(root, './packages/backend/src/server/web/cli.css'),
+		path.join(root, './packages/backend/src/server/web/error.css'),
 	]) {
 		const source = await fs.readFile(file, { encoding: 'utf-8' });
 		const { css } = await postcss([cssnano({ zindex: false })]).process(source, { from: undefined });
-		await fs.writeFile(`./packages/backend/built/server/web/${path.basename(file)}`, css);
+		await fs.writeFile(
+			path.join(root, './packages/backend/built/server/web/', path.basename(file)),
+			css,
+		);
 	}
 }
 
