7f541bbe0c36f1d10333e7b6059ac847f6875dca
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2025-10-22 08:12:26 +0900

    backend: アンテナのインポートでのバリデーションをAjvではなくZodで行うように

--- packages/backend/src/queue/processors/ImportAntennasProcessorService.ts
+++ packages/backend/src/queue/processors/ImportAntennasProcessorService.ts
@@ -4,7 +4,7 @@
  */
 
 import { Injectable, Inject } from '@nestjs/common';
-import Ajv from 'ajv';
+import z from 'zod';
 import { IdService } from '@/core/IdService.js';
 import { GlobalEventService } from '@/core/GlobalEventService.js';
 import { Logger } from '@/logger.js';
@@ -14,38 +14,18 @@ import { QueueLoggerService } from '../QueueLoggerService.js';
 import type { DBAntennaImportJobData } from '../types.js';
 import type * as Bull from 'bullmq';
 
-const validate = new Ajv().compile({
-	type: 'object',
-	properties: {
-		name: { type: 'string', minLength: 1, maxLength: 100 },
-		src: { type: 'string', enum: ['home', 'all', 'users', 'list'] },
-		userListAccts: {
-			type: 'array',
-			items: {
-				type: 'string',
-			},
-			nullable: true,
-		},
-		keywords: { type: 'array', items: {
-			type: 'array', items: {
-				type: 'string',
-			},
-		} },
-		excludeKeywords: { type: 'array', items: {
-			type: 'array', items: {
-				type: 'string',
-			},
-		} },
-		users: { type: 'array', items: {
-			type: 'string',
-		} },
-		caseSensitive: { type: 'boolean' },
-		localOnly: { type: 'boolean' },
-		withReplies: { type: 'boolean' },
-		withFile: { type: 'boolean' },
-		notify: { type: 'boolean' },
-	},
-	required: ['name', 'src', 'keywords', 'excludeKeywords', 'users', 'caseSensitive', 'withReplies', 'withFile', 'notify'],
+const validate = z.object({
+	name: z.string().min(1).max(100),
+	src: z.enum(['home', 'all', 'users', 'list']),
+	userListAccts: z.string().array().nullable().optional(),
+	keywords: z.string().array().array(),
+	excludeKeywords: z.string().array().array(),
+	users: z.string().array(),
+	caseSensitive: z.boolean(),
+	localOnly: z.boolean().default(false),
+	withReplies: z.boolean(),
+	withFile: z.boolean(),
+	notify: z.boolean(),
 });
 
 @Injectable()
@@ -66,12 +46,18 @@ export class ImportAntennasProcessorService {
 	public async process(job: Bull.Job<DBAntennaImportJobData>): Promise<void> {
 		const now = new Date();
 		try {
-			for (const antenna of job.data.antenna) {
-				if (antenna.keywords.length === 0 || antenna.keywords[0].every(x => x === '')) continue;
-				if (!validate(antenna)) {
+			for (const raw of job.data.antenna) {
+				const { success, data: antenna } = validate.safeParse(raw);
+
+				if (!success) {
 					this.logger.warn('Validation Failed');
 					continue;
 				}
+
+				if (antenna.keywords.length === 0 || antenna.keywords[0].every(x => x === '')) {
+					continue;
+				}
+
 				const result = await this.antennasRepository.insert({
 					id: this.idService.gen(now.getTime()),
 					lastUsedAt: now,
@@ -81,17 +67,18 @@ export class ImportAntennasProcessorService {
 					userListId: null,
 					keywords: antenna.keywords,
 					excludeKeywords: antenna.excludeKeywords,
-					users: (antenna.src === 'list' && antenna.userListAccts !== null ? antenna.userListAccts : antenna.users).filter(Boolean),
+					users: (antenna.src === 'list' && antenna.userListAccts != null ? antenna.userListAccts : antenna.users).filter((x) => x),
 					caseSensitive: antenna.caseSensitive,
 					localOnly: antenna.localOnly,
 					withReplies: antenna.withReplies,
 					withFile: antenna.withFile,
 					notify: antenna.notify,
 				}).then(x => this.antennasRepository.findOneByOrFail(x.identifiers[0]));
-				this.logger.succ('Antenna created: ' + result.id);
+
+				this.logger.succ(`Antenna created: ${result.id}`);
 				this.globalEventService.publishInternalEvent('antennaCreated', result);
 			}
-		} catch (err: any) {
+		} catch (err: unknown) {
 			this.logger.error(err);
 		}
 	}
