c34889450dbd116ab2d7aca720108b9afbc696c8
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2025-10-23 16:22:45 +0900

    backend: `secureRndstr()`がセキュアではない問題を修正

--- packages/backend/src/misc/secure-rndstr.ts
+++ packages/backend/src/misc/secure-rndstr.ts
@@ -14,10 +14,7 @@ export function secureRndstr(length = 32, { chars = LU_CHARS } = {}): string {
 	let str = '';
 
 	for (let i = 0; i < length; i++) {
-		let rand = Math.floor((crypto.randomBytes(1).readUInt8(0) / 0xFF) * chars_len);
-		if (rand === chars_len) {
-			rand = chars_len - 1;
-		}
+		const rand = crypto.randomInt(chars_len);
 		str += chars.charAt(rand);
 	}
 
