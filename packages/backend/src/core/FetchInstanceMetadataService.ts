951e6b2b20375b4f2b1f8d48a2d2d5880adca2ec
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2025-10-27 12:28:10 +0900

    backend: refactor `FetchInstanceMetadataService.ts`

--- packages/backend/src/core/FetchInstanceMetadataService.ts
+++ packages/backend/src/core/FetchInstanceMetadataService.ts
@@ -15,25 +15,48 @@ import { LoggerService } from '@/core/LoggerService.js';
 import { HttpRequestService } from '@/core/HttpRequestService.js';
 import { FederatedInstanceService } from '@/core/FederatedInstanceService.js';
 import type { DOMWindow } from 'jsdom';
+import z from 'zod';
+import * as ms from '@/misc/ms.js';
 
-type NodeInfo = {
-	openRegistrations?: unknown;
-	software?: {
-		name?: unknown;
-		version?: unknown;
-	};
-	metadata?: {
-		name?: unknown;
-		nodeName?: unknown;
-		nodeDescription?: unknown;
-		description?: unknown;
-		maintainer?: {
-			name?: unknown;
-			email?: unknown;
-		};
-		themeColor?: unknown;
-	};
-};
+// 互換性維持のためできるだけ緩く
+const WellKnownSchema = z.object({
+	links: z.object({
+		rel: z.unknown(),
+		href: z.string(),
+	}).array(),
+});
+
+// 互換性維持のためできるだけ緩く
+const NodeInfoSchema = z.object({
+	openRegistrations: z.boolean().nullish(),
+	software: z.object({
+		name: z.unknown().nullish(),
+		version: z.string().nullish(),
+	}).nullish(),
+	metadata: z.object({
+		name: z.unknown().nullish(),
+		nodeName: z.unknown().nullish(),
+		nodeDescription: z.unknown().nullish(),
+		description: z.unknown().nullish(),
+		maintainer: z.object({
+			name: z.string().nullish(),
+			email: z.string().nullish(),
+		}).nullish(),
+		themeColor: z.string().nullish(),
+	}).nullish(),
+});
+
+// 互換性維持のためできるだけ緩く
+const ManifestSchema = z.object({
+	icons: z.object({
+		src: z.string().nullish(),
+	}).array().nullish(),
+	theme_color: z.string().nullish(),
+	name: z.string().nullish(),
+	short_name: z.string().nullish(),
+});
+
+type NodeInfo = z.output<typeof NodeInfoSchema>;
 
 @Injectable()
 export class FetchInstanceMetadataService {
@@ -49,24 +72,26 @@ export class FetchInstanceMetadataService {
 		this.logger = this.loggerService.getLogger('metadata', 'cyan');
 	}
 
-	public async tryLock(host: string): Promise<boolean> {
+	private async tryLock(host: string): Promise<boolean> {
 		const mutex = await this.redisClient.set(`fetchInstanceMetadata:mutex:${host}`, '1', 'GET');
 		return mutex !== '1';
 	}
 
-	public unlock(host: string): Promise<'OK'> {
-		return this.redisClient.set(`fetchInstanceMetadata:mutex:${host}`, '0');
+	private async unlock(host: string): Promise<void> {
+		await this.redisClient.set(`fetchInstanceMetadata:mutex:${host}`, '0');
 	}
 
 	public async fetchInstanceMetadata(instance: MiInstance, force = false): Promise<void> {
 		const host = instance.host;
+
 		// Acquire mutex to ensure no parallel runs
 		if (!await this.tryLock(host)) return;
+
 		try {
 			if (!force) {
 				const _instance = await this.federatedInstanceService.fetch(host);
 				const now = Date.now();
-				if (_instance && _instance.infoUpdatedAt && (now - _instance.infoUpdatedAt.getTime() < 1000 * 60 * 60 * 24)) {
+				if (_instance.infoUpdatedAt !== null && (now - _instance.infoUpdatedAt.getTime() < ms.days(1))) {
 					// unlock at the finally caluse
 					return;
 				}
@@ -80,7 +105,7 @@ export class FetchInstanceMetadataService {
 				this.fetchManifest(instance).catch(() => null),
 			]);
 
-			const [favicon, icon, themeColor, name, description] = await Promise.all([
+			const [faviconUrl, icon, themeColor, name, description] = await Promise.all([
 				this.fetchFaviconUrl(instance, dom).catch(() => null),
 				this.fetchIconUrl(instance, dom, manifest).catch(() => null),
 				this.getThemeColor(info, dom, manifest).catch(() => null),
@@ -92,21 +117,19 @@ export class FetchInstanceMetadataService {
 
 			const updates = {
 				infoUpdatedAt: new Date(),
-			} as Record<string, any>;
-
-			if (info) {
-				updates.softwareName = typeof info.software?.name === 'string' ? info.software.name.toLowerCase() : '?';
-				updates.softwareVersion = info.software?.version;
-				updates.openRegistrations = info.openRegistrations;
-				updates.maintainerName = info.metadata ? info.metadata.maintainer ? (info.metadata.maintainer.name ?? null) : null : null;
-				updates.maintainerEmail = info.metadata ? info.metadata.maintainer ? (info.metadata.maintainer.email ?? null) : null : null;
-			}
-
-			if (name) updates.name = name;
-			if (description) updates.description = description;
-			if (icon ?? favicon) updates.iconUrl = (icon && !icon.includes('data:image/png;base64')) ? icon : favicon;
-			if (favicon) updates.faviconUrl = favicon;
-			if (themeColor) updates.themeColor = themeColor;
+				...(info !== null ? {
+					softwareName: typeof info.software?.name === 'string' ? info.software?.name?.toLowerCase() : '?',
+					softwareVersion: info.software?.version ?? null,
+					openRegistrations: info.openRegistrations ?? null,
+					maintainerName: info.metadata?.maintainer?.name ?? null,
+					maintainerEmail: info.metadata?.maintainer?.email ?? null,
+				} : {}),
+				...(name ? { name } : {}),
+				...(description ? { description } : {}),
+				...(icon ?? faviconUrl ? { iconUrl: (icon && !icon.includes('data:image/png;base64')) ? icon : faviconUrl } : {}),
+				...(faviconUrl ? { faviconUrl } : {}),
+				...(themeColor ? { themeColor } : {}),
+			};
 
 			await this.federatedInstanceService.update(instance.id, updates);
 
@@ -122,18 +145,20 @@ export class FetchInstanceMetadataService {
 		this.logger.info(`Fetching nodeinfo of ${instance.host} ...`);
 
 		try {
-			const wellknown = await this.httpRequestService.getJson('https://' + instance.host + '/.well-known/nodeinfo')
-				.catch((err: unknown) => {
-					if (err.statusCode === 404) {
-						throw new Error('No nodeinfo provided');
-					} else {
-						throw err.statusCode ?? err.message;
-					}
-				}) as Record<string, unknown>;
+			let _wellknown: unknown;
 
-			if (wellknown.links == null || !Array.isArray(wellknown.links)) {
-				throw new Error('No wellknown links');
+			try {
+				_wellknown = await this.httpRequestService.getJson(`https://${instance.host}/.well-known/nodeinfo`);
+			} catch (err: unknown) {
+				if (err.statusCode === 404) {
+					throw new Error('No nodeinfo provided');
+				} else {
+					throw err.statusCode ?? err.message;
+				}
 			}
+
+			const wellknown = (await WellKnownSchema.safeParseAsync(_wellknown)).data;
+			if (wellknown === undefined) throw new Error('invalid wellknown');
 
 			const links = wellknown.links;
 
@@ -142,18 +167,24 @@ export class FetchInstanceMetadataService {
 			const link2_1 = links.find(link => link.rel === 'http://nodeinfo.diaspora.software/ns/schema/2.1');
 			const link = link2_1 ?? link2_0 ?? link1_0;
 
-			if (link == null) {
+			if (link === undefined) {
 				throw new Error('No nodeinfo link provided');
 			}
 
-			const info = await this.httpRequestService.getJson(link.href)
-				.catch((err: unknown) => {
-					throw err.statusCode ?? err.message;
-				});
+			let _nodeinfo: unknown;
+
+			try {
+				_nodeinfo = await this.httpRequestService.getJson(link.href);
+			} catch (err) {
+				throw err.statusCode ?? err.message;
+			}
 
 			this.logger.succ(`Successfuly fetched nodeinfo of ${instance.host}`);
 
-			return info as NodeInfo;
+			const nodeinfo = (await NodeInfoSchema.safeParseAsync(_nodeinfo)).data;
+			if (nodeinfo === undefined) throw new Error('invalid nodeinfo');
+
+			return nodeinfo;
 		} catch (err) {
 			this.logger.error(`Failed to fetch nodeinfo of ${instance.host}: ${err}`);
 
@@ -164,7 +195,7 @@ export class FetchInstanceMetadataService {
 	private async fetchDom(instance: MiInstance): Promise<DOMWindow['document']> {
 		this.logger.info(`Fetching HTML of ${instance.host} ...`);
 
-		const url = 'https://' + instance.host;
+		const url = `https://${instance.host}`;
 
 		const html = await this.httpRequestService.getHtml(url);
 
@@ -174,18 +205,18 @@ export class FetchInstanceMetadataService {
 		return doc;
 	}
 
-	private async fetchManifest(instance: MiInstance): Promise<Record<string, unknown> | null> {
-		const url = 'https://' + instance.host;
+	private async fetchManifest(instance: MiInstance): Promise<z.output<typeof ManifestSchema> | null> {
+		const url = `https://${instance.host}`;
 
-		const manifestUrl = url + '/manifest.json';
+		const manifestUrl = `${url}/manifest.json`;
 
 		const manifest = await this.httpRequestService.getJson(manifestUrl);
 
-		return manifest;
+		return ManifestSchema.nullable().parse(manifest);
 	}
 
 	private async fetchFaviconUrl(instance: MiInstance, doc: DOMWindow['document'] | null): Promise<string | null> {
-		const url = 'https://' + instance.host;
+		const url = `https://${instance.host}`;
 
 		if (doc) {
 			// https://github.com/misskey-dev/misskey/pull/8220#issuecomment-1025104043
@@ -196,7 +227,7 @@ export class FetchInstanceMetadataService {
 			}
 		}
 
-		const faviconUrl = url + '/favicon.ico';
+		const faviconUrl = `${url}/favicon.ico`;
 
 		const favicon = await this.httpRequestService.send(faviconUrl, {
 			method: 'HEAD',
@@ -209,14 +240,14 @@ export class FetchInstanceMetadataService {
 		return null;
 	}
 
-	private async fetchIconUrl(instance: MiInstance, doc: DOMWindow['document'] | null, manifest: Record<string, any> | null): Promise<string | null> {
-		if (manifest && manifest.icons && manifest.icons.length > 0 && manifest.icons[0].src) {
-			const url = 'https://' + instance.host;
-			return (new URL(manifest.icons[0].src, url)).href;
+	private async fetchIconUrl(instance: MiInstance, doc: DOMWindow['document'] | null, manifest: z.output<typeof ManifestSchema> | null): Promise<string | null> {
+		if (manifest?.icons && manifest.icons.length > 0 && manifest.icons[0]?.src) {
+			const baseurl = `https://${instance.host}`;
+			return (new URL(manifest.icons[0].src, baseurl)).href;
 		}
 
 		if (doc) {
-			const url = 'https://' + instance.host;
+			const url = `https://${instance.host}`;
 
 			// https://github.com/misskey-dev/misskey/pull/8220#issuecomment-1025104043
 			const links = Array.from(doc.getElementsByTagName('link')).reverse();
@@ -237,7 +268,7 @@ export class FetchInstanceMetadataService {
 		return null;
 	}
 
-	private async getThemeColor(info: NodeInfo | null, doc: DOMWindow['document'] | null, manifest: Record<string, any> | null): Promise<string | null> {
+	private async getThemeColor(info: NodeInfo | null, doc: DOMWindow['document'] | null, manifest: z.output<typeof ManifestSchema> | null): Promise<string | null> {
 		const themeColor = info?.metadata?.themeColor ?? doc?.querySelector('meta[name="theme-color"]')?.getAttribute('content') ?? manifest?.theme_color;
 
 		if (themeColor) {
@@ -248,8 +279,8 @@ export class FetchInstanceMetadataService {
 		return null;
 	}
 
-	private async getSiteName(info: NodeInfo | null, doc: DOMWindow['document'] | null, manifest: Record<string, any> | null): Promise<string | null> {
-		if (info && info.metadata) {
+	private async getSiteName(info: NodeInfo | null, doc: DOMWindow['document'] | null, manifest: z.output<typeof ManifestSchema> | null): Promise<string | null> {
+		if (info?.metadata) {
 			if (typeof info.metadata.nodeName === 'string') {
 				return info.metadata.nodeName;
 			} else if (typeof info.metadata.name === 'string') {
@@ -266,14 +297,14 @@ export class FetchInstanceMetadataService {
 		}
 
 		if (manifest) {
-			return manifest.name ?? manifest.short_name;
+			return manifest.name ?? manifest.short_name ?? null;
 		}
 
 		return null;
 	}
 
-	private async getDescription(info: NodeInfo | null, doc: DOMWindow['document'] | null, manifest: Record<string, any> | null): Promise<string | null> {
-		if (info && info.metadata) {
+	private async getDescription(info: NodeInfo | null, doc: DOMWindow['document'] | null, manifest: z.output<typeof ManifestSchema> | null): Promise<string | null> {
+		if (info?.metadata) {
 			if (typeof info.metadata.nodeDescription === 'string') {
 				return info.metadata.nodeDescription;
 			} else if (typeof info.metadata.description === 'string') {
@@ -294,7 +325,7 @@ export class FetchInstanceMetadataService {
 		}
 
 		if (manifest) {
-			return manifest.name ?? manifest.short_name;
+			return manifest.name ?? manifest.short_name ?? null;
 		}
 
 		return null;
