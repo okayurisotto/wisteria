7799903478eb55e46d9d4cbc3cd07f0c73d54632
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2026-04-26 23:34:17 +0900

    backend: 内製の redis-lock を使うように

--- packages/backend/src/core/AppLockService.ts
+++ packages/backend/src/core/AppLockService.ts
@@ -3,39 +3,33 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { promisify } from 'node:util';
 import { Inject, Injectable } from '@nestjs/common';
-import redisLock from 'redis-lock';
+import { acquireDistributedLock } from 'redis-lock';
 import * as Redis from 'ioredis';
 import { DI } from '@/di-symbols.js';
 
-/**
- * Retry delay (ms) for lock acquisition
- */
-const retryDelay = 100;
-
 @Injectable()
 export class AppLockService {
-	private readonly lock: (key: string, timeout?: number, _?: (() => Promise<void>) | undefined) => Promise<() => void>;
-
 	constructor(
 		@Inject(DI.redis)
 		private readonly redisClient: Redis.Redis,
 	) {
-		this.lock = promisify(redisLock(this.redisClient, retryDelay));
 	}
 
 	/**
 	 * Get AP Object lock
 	 * @param uri AP object ID
-	 * @param timeout Lock timeout (ms), The timeout releases previous lock.
 	 * @returns Unlock function
 	 */
-	public getApLock(uri: string, timeout = 30 * 1000): Promise<() => void> {
-		return this.lock(`ap-object:${uri}`, timeout);
-	}
-
-	public getChartInsertLock(lockKey: string, timeout = 30 * 1000): Promise<() => void> {
-		return this.lock(`chart-insert:${lockKey}`, timeout);
+	public async getApLock(uri: string): Promise<() => void> {
+		const lock = await acquireDistributedLock(
+			this.redisClient,
+			`ap-object:${uri}`,
+			30 * 1000,
+			100,
+			50,
+			undefined,
+		);
+		return lock.release;
 	}
 }
