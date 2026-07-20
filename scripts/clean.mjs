fb98063f7bbbfeb27db5091f50f59f1184746635
okayurisotto <47853651+okayurisotto@users.noreply.github.com>
2026-07-20 17:53:28 +0900

    update `scripts/clean.mjs` for redis-lock

--- scripts/clean.mjs
+++ scripts/clean.mjs
@@ -17,6 +17,7 @@ void Promise.all([
 	rm(resolve(import.meta.dirname, '../packages/locales/built'), { recursive: true, force: true }),
 	rm(resolve(import.meta.dirname, '../packages/misskey-js/built'), { recursive: true, force: true }),
 	rm(resolve(import.meta.dirname, '../packages/parcom/built'), { recursive: true, force: true }),
+	rm(resolve(import.meta.dirname, '../packages/redis-lock/built'), { recursive: true, force: true }),
 	rm(resolve(import.meta.dirname, '../packages/sw/built'), { recursive: true, force: true }),
 	rm(resolve(import.meta.dirname, '../packages/zod2spec/built'), { recursive: true, force: true }),
 
@@ -30,6 +31,7 @@ void Promise.all([
 	rm(resolve(import.meta.dirname, '../packages/locales/.turbo'), { recursive: true, force: true }),
 	rm(resolve(import.meta.dirname, '../packages/misskey-js/.turbo'), { recursive: true, force: true }),
 	rm(resolve(import.meta.dirname, '../packages/parcom/.turbo'), { recursive: true, force: true }),
+	rm(resolve(import.meta.dirname, '../packages/redis-lock/.turbo'), { recursive: true, force: true }),
 	rm(resolve(import.meta.dirname, '../packages/sw/.turbo'), { recursive: true, force: true }),
 	rm(resolve(import.meta.dirname, '../packages/zod2spec/.turbo'), { recursive: true, force: true }),
 ]);
